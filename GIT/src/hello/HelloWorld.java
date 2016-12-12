80e619cd8cc4d980e74e3f8419e1ad6cf366c6a4
Bob Oldengarm <Bob Oldengarm@LAPTOP-76OEILV1>
2016-12-12 11:41:50 +0100

    Revert "Lekker rekenen"
    This reverts commit de721efca7eb06968c7630f7bca9d335199634a5.

--- GIT/src/hello/HelloWorld.java
+++ GIT/src/hello/HelloWorld.java
@@ -2,13 +2,7 @@ package hello;
 
 public class HelloWorld {
 	
-	public int c;
 	public static void main(String[] args) {
 		System.out.println("PogChamp123");
-	}
-	
-	public int addition(int a, int b) {
-		c = a + b;
-		return c;
 	}
 }  
