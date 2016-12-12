de721efca7eb06968c7630f7bca9d335199634a5
Bob Oldengarm <Bob Oldengarm@LAPTOP-76OEILV1>
2016-12-12 11:38:34 +0100

    Lekker rekenen

--- GIT/src/hello/HelloWorld.java
+++ GIT/src/hello/HelloWorld.java
@@ -2,7 +2,13 @@ package hello;
 
 public class HelloWorld {
 	
+	public int c;
 	public static void main(String[] args) {
 		System.out.println("PogChamp123");
+	}
+	
+	public int addition(int a, int b) {
+		c = a + b;
+		return c;
 	}
 }  
